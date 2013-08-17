0224aa31c335956520a801f47c8f62a8435dea3b
robin <robin@robinkirkman.com>
2013-08-17 09:09:22 -0400

    Change kryomq version to 1.0.0-SNAPSHOT

--- kryomq/src/main/java/org/kryomq/MqServer.java
+++ kryomq/src/main/java/org/kryomq/MqServer.java
@@ -6,8 +6,6 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.kryomq.Meta.MetaType;
