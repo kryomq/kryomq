934547517c66bf38769d801438cf4f9b8529ccc0
robin <robin@robinkirkman.com>
2013-08-17 13:19:57 -0400

    Disable logging output by default even when using slf4j

--- kryo/src/main/java/org/kryomq/kryo/Log.java
+++ kryo/src/main/java/org/kryomq/kryo/Log.java
@@ -7,7 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Log {
-	private static final Logger log = LoggerFactory.getLogger("org.kryomq.kryo");
+	private static final Logger log = LoggerFactory.getLogger(Log.class.getPackage().getName());
 	
 	private static Map<String, Logger> logs = new ConcurrentHashMap<String, Logger>();
 	
@@ -15,33 +15,52 @@ public class Log {
 		Logger log = logs.get(category);
 		if(log != null)
 			return log;
-		logs.put(category, log = LoggerFactory.getLogger(Kryo.class.getName() + "." + category));
+		logs.put(category, log = LoggerFactory.getLogger(Log.class.getPackage().getName() + "." + category));
 		return log;
 	}
 	
-	public static final boolean TRACE = log.isTraceEnabled();
-	public static final boolean DEBUG = log.isDebugEnabled();
-	public static final boolean INFO = log.isInfoEnabled();
-	public static final boolean WARN = log.isWarnEnabled();
-	public static final boolean ERROR = log.isErrorEnabled();
+	public static boolean TRACE = log.isTraceEnabled();
+	public static boolean DEBUG = log.isDebugEnabled();
+	public static boolean INFO = log.isInfoEnabled();
+	public static boolean WARN = log.isWarnEnabled();
+	public static boolean ERROR = log.isErrorEnabled();
+	
+	private static boolean off;
+	
+	public static void off() {
+		Log.off = true;
+	}
+	
+	public static void on() {
+		Log.off = false;
+	}
+	
+	static {
+		off();
+	}
 	
 	public static void trace(String category, String format, Object... arguments) {
+		if(off) return;
 		cat(category).trace(format, arguments);
 	}
 	
 	public static void debug(String category, String format, Object... arguments) {
+		if(off) return;
 		cat(category).debug(format, arguments);
 	}
 	
 	public static void info(String category, String format, Object... arguments) {
+		if(off) return;
 		cat(category).info(format, arguments);
 	}
 	
 	public static void warn(String category, String format, Object... arguments) {
+		if(off) return;
 		cat(category).warn(format, arguments);
 	}
 	
 	public static void error(String category, String format, Object... arguments) {
+		if(off) return;
 		cat(category).error(format, arguments);
 	}
 	
