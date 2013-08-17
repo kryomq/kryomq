9e42424fee2f11c3c21d479a5f3fda0a72e24173
robin <robin@robinkirkman.com>
2013-08-17 15:49:23 -0400

    Make Topics an interface

--- kryomq/src/main/java/org/kryomq/mq/Topics.java
+++ kryomq/src/main/java/org/kryomq/mq/Topics.java
@@ -1,12 +1,10 @@
 package org.kryomq.mq;
 
-public class Topics {
+public interface Topics {
 
 	public static final String PRIVILEGED = "privileged.";
 	public static final String CONTROLLED = "controlled.";
 	
 	public static final String CLIENT = "client.";
 	
-
-	private Topics() {}
 }
