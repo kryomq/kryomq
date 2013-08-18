65dda3414edcdccb7d221bbc7301bd3a70d469aa
robin <robin@robinkirkman.com>
2013-08-17 20:33:47 -0400

    More commenting

--- kryomq/src/main/java/org/kryomq/mq/Control.java
+++ kryomq/src/main/java/org/kryomq/mq/Control.java
@@ -1,19 +1,60 @@
 package org.kryomq.mq;
 
+/**
+ * An object representing an action taken by an {@link MqClient} that must be sent
+ * to the {@link MqServer}.
+ * @author robin
+ *
+ */
 public class Control {
+	/**
+	 * The command types available to an {@link MqClient}
+	 * @author robin
+	 *
+	 */
 	public static enum Command {
+		/**
+		 * Subscribe to a topic
+		 */
 		SUBSCRIBE,
+		/**
+		 * Unsubscribe from a topic
+		 */
 		UNSUBSCRIBE,
+		/**
+		 * Set the local origin topic
+		 */
 		SET_ORIGIN,
+		/**
+		 * Turn on queueing for a topic
+		 */
 		SET_QUEUE,
+		/**
+		 * Turn off queueing for a topic
+		 */
 		UNSET_QUEUE,
 	}
 	
+	/**
+	 * The command type
+	 */
 	public Command command;
+	/**
+	 * The topic the command is applied to
+	 */
 	public String topic;
 	
+	/**
+	 * required for deserialization
+	 */
+	@Deprecated
 	public Control() {}
 	
+	/**
+	 * Create a new {@link MqClient} control command
+	 * @param command The command type
+	 * @param topic The topic applied to
+	 */
 	public Control(Command command, String topic) {
 		this.command = command;
 		this.topic = topic;
--- kryomq/src/main/java/org/kryomq/mq/Meta.java
+++ kryomq/src/main/java/org/kryomq/mq/Meta.java
@@ -1,19 +1,59 @@
 package org.kryomq.mq;
 
+/**
+ * A statement about the KryoMQ system made by an {@link MqServer} to an {@link MqClient}
+ * @author robin
+ *
+ */
 public class Meta {
+	/**
+	 * Types of KryoMQ metadata
+	 * @author robin
+	 *
+	 */
 	public static enum MetaType {
+		/**
+		 * Assign a unique numeric {@link MqClient} id
+		 */
 		ID,
+		/**
+		 * Assign a unique privileged topic.  See {@link Topics#PRIVILEGED}.
+		 */
 		PERSONAL_TOPIC,
+		/**
+		 * Assign a unique controlled topic. See {@link Topics#CONTROLLED}
+		 */
 		CONTROLLED_TOPIC,
+		/**
+		 * Some other {@link MqClient} has connected
+		 */
 		CONNECTED,
+		/**
+		 * Some other {@link MqClient} has disconnected
+		 */
 		DISCONNECTED,
 	}
 	
+	/**
+	 * The metadata type
+	 */
 	public MetaType type;
+	/**
+	 * The metadata topic, either beign assigned or the personal topic of the {@link MqClient} in question
+	 */
 	public String topic;
 	
+	/**
+	 * required for deserialization
+	 */
+	@Deprecated
 	public Meta() {}
 	
+	/**
+	 * Create a new metadata message
+	 * @param type
+	 * @param topic
+	 */
 	public Meta(MetaType type, String topic) {
 		this.type = type;
 		this.topic = topic;
